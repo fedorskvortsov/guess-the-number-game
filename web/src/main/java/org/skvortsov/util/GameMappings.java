a5610f571b229fda50fbeeb1d3cb47505e7b8deb
fedorskvortsov <skvortsovfk@gmail.com>
2022-03-05 15:51:23 +0300

    feat: play Thymeleaf Template

---
+++ web/src/main/java/org/skvortsov/util/GameMappings.java
@@ -0,0 +1,9 @@
+package org.skvortsov.util;
+
+public final class GameMappings {
+
+    public static final String PLAY = "play";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+
+    private GameMappings() {}
+}
--- web/src/main/java/org/skvortsov/util/ViewNames.java
+++ web/src/main/java/org/skvortsov/util/ViewNames.java
@@ -3,6 +3,8 @@ package org.skvortsov.util;
 public final class ViewNames {
 
     public final static String HOME = "home";
+    public static final String PLAY = "play";
+    public static final String GAME_OVER = "game-over";
 
     private ViewNames() {}
 }
