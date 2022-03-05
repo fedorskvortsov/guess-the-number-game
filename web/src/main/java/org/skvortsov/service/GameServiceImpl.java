6a5c29cb9c8673505823659fe6b3330e46fe9784
fedorskvortsov <skvortsovfk@gmail.com>
2022-03-05 15:29:34 +0300

    feat: add game service

---
+++ web/src/main/java/org/skvortsov/service/GameServiceImpl.java
@@ -0,0 +1,54 @@
+package org.skvortsov.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.skvortsov.main.Game;
+import org.skvortsov.main.MessageGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Service
+public class GameServiceImpl implements GameService {
+    private final Game game;
+    private final MessageGenerator messageGenerator;
+
+    @Autowired
+    public GameServiceImpl(Game game, MessageGenerator messageGenerator) {
+        this.game = game;
+        this.messageGenerator = messageGenerator;
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("number = {}", game.getNumber());
+        log.info("mainMessage = {}", messageGenerator.getMainMessage());
+    }
+
+    @Override
+    public boolean isGameOver() {
+        return game.isGameLost() || game.isGameWon();
+    }
+
+    @Override
+    public String getMainMessage() {
+        return messageGenerator.getMainMessage();
+    }
+
+    @Override
+    public String getResultMessage() {
+        return messageGenerator.getResultMessage();
+    }
+
+    @Override
+    public void checkGuess(int guess) {
+        game.setGuess(guess);
+        game.check();
+    }
+
+    @Override
+    public void reset() {
+        game.reset();
+    }
+}
